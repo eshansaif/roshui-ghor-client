b98ca0227f864479b953a1a0080566f791610314
eshansaif <eshansaif1234@gmail.com>
2024-06-04 05:00:12 +0600

    navbar is updated

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -4,21 +4,31 @@ import banner2 from "../../assets/banner.jpg";
 export default function Banner() {
   return (
     <div
-      className="hero min-h-[400px] "
+      data-aos="fade-up"
+      data-aos-offset="200"
+      // data-aos-delay="50"
+      // data-aos-duration="1000"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="false"
+      data-aos-anchor-placement="top-center"
+      className="hero min-h-[400px]"
       style={{
-        backgroundImage: `url(${banner2})`,
+        backgroundImage: `url("https://d3ox4wjkl7mf3m.cloudfront.net/feed_story/step/yCGo6Rzz3nsf4n9WrquZb3gEHxjaqEZqH5zhSo8h.jpeg")`,
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-start text-neutral-content">
-        <div className="max-w-md text-center">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5 ">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Cooking Is Science</h1>
+          <p className="mb-5">
+            Good cooks should know something about the Maillard reaction,
+            electromagnetic waves, and the way that proline-rich proteins bond
+            with polyphenols. Don’t worry. This isn’t that dream where you walk
+            into the chemistry final and realize you have never attended a day
+            of class. We make it easy for you.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">Explore More</button>
         </div>
       </div>
     </div>
